1cb8909c120eecdf42a48b6498d3c31ce80b403d
Ricardo Monteiro <ricardo@lab12.com.br>
2017-07-06 15:15:02 -0300

    changes in structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,62 @@
-var elixir = require('laravel-elixir');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var minify = require('gulp-clean-css');
 
-elixir.config.sourcemaps = false;
-elixir.config.assetsPath = 'src';
-elixir.config.publicPath = 'assets';
+// var scripts = [
+//     "src/_js/Core/Kube.js",
+//     "src/_js/Core/Kube.Plugin.js",
+//     "src/_js/Core/Kube.Animation.js",
+//     "src/_js/Core/Kube.Detect.js",
+//     "src/_js/Core/Kube.FormData.js",
+//     "src/_js/Core/Kube.Response.js",
+//     "src/_js/Core/Kube.Utils.js",
+//     "src/_js/Message/Kube.Message.js",
+//     "src/_js/Sticky/Kube.Sticky.js",
+//     "src/_js/Toggleme/Kube.Toggleme.js",
+//     "src/_js/Offcanvas/Kube.Offcanvas.js",
+//     "src/_js/Collapse/Kube.Collapse.js",
+//     "src/_js/Dropdown/Kube.Dropdown.js",
+//     "src/_js/Tabs/Kube.Tabs.js",
+//     "src/_js/Modal/Kube.Modal.js"
+// ];
 
-elixir(function(mix) {
+gulp.task('sass', function() {
+    return gulp.src('src/monalisa.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('dist/css'))
+        .pipe(rename('monalisa.min.css'))
+        .pipe(minify())
+        .pipe(gulp.dest('dist/css'));
+});
 
-    mix.sass('monalisa.scss');
-    mix.sass('./docs/docs.scss', './docs/docs.css');
+gulp.task('combine', function() {
+    return gulp.src([
+            'src/_scss/_variables.scss',
+            'src/_scss/mixins/*.scss',
+            'src/_scss/components/*.scss'
+        ])
+        .pipe(concat('monalisa.scss'))
+        .pipe(gulp.dest('dist/scss'));
+});
+
+// gulp.task('scripts', function() {
+//     return gulp.src(scripts)
+//         .pipe(concat('kube.js'))
+//         .pipe(gulp.dest('dist/js'))
+//         .pipe(rename('kube.min.js'))
+//         .pipe(uglify())
+//         .pipe(gulp.dest('dist/js'));
+// });
+
+gulp.task('watch', function() {
+
+    // gulp.watch(scripts, ['scripts']);
+    gulp.watch(['src/_scss/*.scss', 'src/_scss/components/*.scss', 'src/_scss/mixins/*.scss'], ['sass', 'combine']);
 
 });
+
+// gulp.task('default', ['sass', 'combine', 'scripts', 'watch']);
+gulp.task('default', ['sass', 'combine']);
