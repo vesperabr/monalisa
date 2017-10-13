d51c7581ba8ff71860dce0a169daf65af7763275
Cassio <cassiohenrique_cahe@hotmail.com>
2017-10-13 14:05:21 -0300

    Pagina Form finalizada

---
+++ docs/data/forms/exe_2.php
@@ -0,0 +1,8 @@
+<div class="example">
+	<form class="Form">
+	    <div class="form-item">
+	        <label>Name</label>
+	        <input type="text">
+	    </div>
+	</form>
+</div>
---
+++ docs/data/forms/exe_8.php
@@ -0,0 +1,21 @@
+<div class="example">
+	<form class="Form">
+		<div class="form-item">
+			<input type="text" value="" class="_disabled">
+		</div>
+		<div class="form-item">
+			<input type="checkbox" class="_disabled">
+		</div>
+		<div class="form-item">
+			<input type="radio" class="_disabled">
+		</div>
+		<div class="form-item">
+			<textarea class="_disabled"></textarea>
+		</div>
+		<div class="form-item">
+			<select class="_disabled">
+			    <option>...</option>
+			</select>
+		</div>
+	</form>
+</div>
