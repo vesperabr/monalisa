3d103da5a7b50162f76497ca336c74f104e14b43
Cassio <cassiohenrique_cahe@hotmail.com>
2017-10-13 16:57:44 -0300

    Pagination Finalizada

--- docs/pagination.php
+++ docs/pagination.php
@@ -2,4 +2,19 @@
 
 	<h1>Pagination</h1>
 
+	<h2>Base</h2>
+
+	<? include 'data/pagination/exe_1.php'; ?>
+	<pre data-src="data/pagination/cod_1.html"></pre>
+
+	<h2>Centered</h2>
+
+	<? include 'data/pagination/exe_2.php'; ?>
+	<pre data-src="data/pagination/cod_2.html"></pre>
+
+	<h2>Pager</h2>
+
+	<? include 'data/pagination/exe_3.php'; ?>
+	<pre data-src="data/pagination/cod_3.html"></pre>
+
 <? include '_footer.php'; ?>
