228f845c251d29ce1cb15ce7fc0d6b8b7ea6eceb
Cassio <cassiohenrique_cahe@hotmail.com>
2017-10-13 09:42:57 -0300

    Paginas Tables feita

---
+++ docs/data/table/exe_6.php
@@ -0,0 +1,23 @@
+<div class="example">
+<table>
+	<thead>
+		<tr>
+			<th class="_w40">First name</th>
+			<th class="_w40">Last name</th>
+			<th class="_w20">Points</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>Giovanni</td>
+			<td>Alonso</td>
+			<td>75</td>
+		</tr>
+		<tr>
+			<td>Clarice</td>
+			<td>Assis</td>
+			<td>30</td>
+		</tr>
+	</tbody>
+</table>
+</div>
