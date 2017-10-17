d7577794a371a65ead6ff61c6f88a899f77c7352
Cassio <cassiohenrique_cahe@hotmail.com>
2017-10-17 10:10:49 -0200

    Correcoes e atualizacoes

--- docs/data/typography/exe_4.php
+++ docs/data/typography/exe_4.php
@@ -1,6 +1,6 @@
 <div class="example">
 	<blockquote>
-	    <p>No, she'll probably make me do it. Goodbye, friends. I never thought I'd die like this. But I always really hoped. I saw you with those two "ladies of the evening" at Elzars. Explain that. I never loved you.</p>
+	    <p>"Well, here at last, dear friends, on the shores of the sea comes the end of our fellowship in middle-earth. Go in peace! I will not say: do not weep for not all tears are an evil."</p>
 	</blockquote>
 </div>
 
